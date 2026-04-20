7ce45f63ac861e69caf149b94f6a90cb36a91db9
weerayut-ak <weerayut4109@gmail.com>
2026-04-20 12:53:02 +0700

    fix: ignore ts and eslint errors on build

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="th" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
       <body suppressHydrationWarning>
         {children}
         <Toaster
